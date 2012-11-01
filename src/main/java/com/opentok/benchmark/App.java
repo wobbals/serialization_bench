8a0ba6022d1a523c21982639430eef04686fa4d5
Charley Robinson <charley@tokbox.com>
2012-11-01 15:48:05 -0700

    who knew classloading was so relevant?

--- src/main/java/com/opentok/benchmark/App.java
+++ src/main/java/com/opentok/benchmark/App.java
@@ -9,9 +9,35 @@ public class App
 {
     public static void main( String[] args ) 
     {
+	for (int i = 0; i < 10; i++) {
+	ResultSet result = runTest();
+	System.out.printf("json parsing completed in %d ns\n", result.jsonParseTime);
+	System.out.printf("json writing completed in %d ns\n", result.jsonWriteTime);
+	System.out.printf("total json test time: %d ns\n", result.jsonParseTime+result.jsonWriteTime);
+
+	System.out.printf("protobuf parsing completed in %d ns\n", result.protobufParseTime);
+	System.out.printf("protobuf writing completed in %d ns\n", result.protobufWriteTime);
+	System.out.printf("total protobuf test time: %d ns\n", result.protobufParseTime+result.protobufWriteTime);
+	System.out.println("-------");
+	System.out.println("json parse / protobuf parse = " + result.jsonParseTime / result.protobufParseTime);
+	System.out.println("json write / protobuf write = " + result.jsonWriteTime / result.protobufWriteTime);
+	System.out.println("total json / total protobuf = " + (result.jsonParseTime + result.jsonWriteTime) / (result.protobufWriteTime + result.protobufParseTime));
+
+	System.out.println("  ");
+	}
+    }
+    
+    public static class ResultSet {
+	long jsonParseTime;
+	long jsonWriteTime;
+	long protobufParseTime;
+	long protobufWriteTime;
+    }
+    
+    public static ResultSet runTest() {
 	try {
-	long startTime = System.nanoTime();
 	String sampleInputString = "{\"type\":4,\"id\":\"2_MX4xMzExMjU3MX43Mi41LjE2Ny4xNTh-VGh1IE9jdCAxOCAxNToxMzoyOCBQRFQgMjAxMn4wLjMzMjY4NDF-_35797926\",\"data\":{\"type\":3,\"action\":1,\"objectKey\":\"2_MX4xMzExMjU3MX43Mi41LjE2Ny4xNTh-VGh1IE9jdCAxOCAxNToxMzoyOCBQRFQgMjAxMn4wLjMzMjY4NDF-\",\"key\":\"connections\",\"value\":{\"connectionsCount\":2,\"currentConnection\":\"35797926\"},\"currentActionId\":77,\"replyExpected\":true}}";
+	long startTime = System.nanoTime();
 	ObjectMapper mapper = new ObjectMapper();
 	JsonNode node = mapper.readTree(sampleInputString);
 	long readTime =  System.nanoTime() ;
@@ -21,9 +47,8 @@ public class App
 	long jsonParseTime = readTime - startTime;
 	long jsonWriteTime = writeTime - readTime;
 	
-	System.out.println("input: "+sampleInputString);
-	System.out.println("output: "+sampleOutputString);
-	
+	//System.out.println("json test: "+sampleInputString);
+		
 	TestObject testObject = 
 	    TestObject.newBuilder()
 	    .setType(TestObject.Type.FOUR)
@@ -41,7 +66,7 @@ public class App
 		     .build()
 		     ).build();
 	
-	System.out.println("testObject: "+testObject.toString());
+	//System.out.println("testObject: "+testObject.toString());
 	startTime = System.nanoTime();
 	byte[] testObjectEncoded = testObject.toByteArray();
 	writeTime = System.nanoTime();
@@ -50,21 +75,18 @@ public class App
 
 	long protobufWriteTime = writeTime - startTime;
 	long protobufParseTime = readTime - writeTime;
-
-	System.out.printf("json parsing completed in %d ns\n", jsonParseTime);
-	System.out.printf("json writing completed in %d ns\n", jsonWriteTime);
-	System.out.printf("total json test time: %d ns\n", jsonParseTime+jsonWriteTime);
-
-	System.out.printf("protobuf parsing completed in %d ns\n", protobufParseTime);
-	System.out.printf("protobuf writing completed in %d ns\n", protobufWriteTime);
-	System.out.printf("total protobuf test time: %d ns\n", protobufParseTime+protobufWriteTime);
-	System.out.println("-------");
-	System.out.println("json parse / protobuf parse = " + jsonParseTime / protobufParseTime);
-	System.out.println("json write / protobuf write = " + jsonWriteTime / protobufWriteTime);
-	System.out.println("total json / total protobuf = " + (jsonParseTime + jsonWriteTime) / (protobufWriteTime + protobufParseTime));
-
+	
+	ResultSet result = new ResultSet();
+	result.protobufWriteTime = protobufWriteTime;
+	result.protobufParseTime = protobufParseTime;
+	result.jsonWriteTime = jsonWriteTime;
+	result.jsonParseTime = jsonParseTime;
+	return result;
+	
 	} catch (Exception e) {
 	    e.printStackTrace();
+	    return null;
 	}
+
     }
 }
